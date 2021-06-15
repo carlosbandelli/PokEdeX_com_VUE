0e711acf9d26a52d16d1e66ed5357d832f82a20f
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-15 15:10:23 -0300

    axios e pokemon intalados

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+
+    </div>
+
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+
+  data(){
+    return {
+      pokemnos: []
+    }
+  },
+
+  created: function(){
+   axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+     console.log("pegou a listade pokemons")
+     this.pokemons = res.data.results
+     
+   })
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
