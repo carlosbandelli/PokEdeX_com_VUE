b65bf64d18c8b989f89b6ca06876452efae67895
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-16 16:06:36 -0300

    compoentes adicionados

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
     <div id="app">
-
+      <div class="column is-half is-offset-one-quarter is-desktop content is-centered is-vcentered">
+        <div v-for="(poke,index) in pokemons" :key="index" class= "is-centered is-vcentered">
+          <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />        
+        </div>
+      </div>
     </div>
-
-
 </template>
 
 <script>
 import axios from 'axios'
-
+import Pokemon from './components/Pokemon'
 export default {
   name: 'App',
 
   data(){
     return {
-      pokemnos: []
+      pokemons: []
     }
   },
 
   created: function(){
    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
-     console.log("pegou a listade pokemons")
+     console.log("Pegou a lista de pokemons!")
      this.pokemons = res.data.results
      
    })
-  }
-  
+  },
+  components:{
+    Pokemon
+  }  
 }
 </script>
 
